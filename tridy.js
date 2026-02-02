4dc296aa68adb767a4d25595605601ea5519ab02
motejlkova <petra.motejlkova154@student.cuni.cz>
2026-02-02 11:06:35 +0100

    Přidány funkce pro přidání/mazání položek, výpis položek a zobrazení aktuálního stavu

--- tridy.js
+++ tridy.js
@@ -4,3 +4,12 @@
             this.typ = typ;
     }
   }
+
+  class Polozka {
+    constructor(nazev, castka, typ, kategorie) {
+        this.nazev = nazev;
+        this.castka = castka;
+        this.typ = typ;
+        this.kategorie = kategorie;
+    }
+}
